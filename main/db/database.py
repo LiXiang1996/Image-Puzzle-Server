524e15ec1799c01fb19170ed22e3b2a8b009f38c
lixiang <lixiang@joyme.sg>
2025-11-27 16:04:46 +0800

    【feat】接口修改，实体类修改

--- main/db/database.py
+++ main/db/database.py
@@ -126,7 +126,7 @@ def init_db():
     # 导入所有模型类
     # 这一步很重要：确保所有模型类被注册到SQLModel.metadata中
     # 如果不导入，SQLModel不知道要创建哪些表
-    from db.models import User, Work, ConsumptionRecord
+    from db.models import User, Note
     
     # 创建所有表
     # SQLModel.metadata.create_all(engine) 会：
